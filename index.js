ad6049ecbe670d4b01b1e36fca07cb6d268be92b
afrinchowa <arfinchowa524@gmail.com>
2025-06-24 23:38:14 +0600

    Basic Project setup for job portal server

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8dojk.mongodb.net/job-seeker?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Job server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
